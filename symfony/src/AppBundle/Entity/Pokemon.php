0e7f8610d339ece3cca15047ba7bcd10b5963169
Alberto Cantón León <albertocantonleon@MacBook-Pro-de-Alberto.local>
2018-05-12 20:10:28 +0200

    Generate Pokemon Entity

---
+++ symfony/src/AppBundle/Entity/Pokemon.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Pokemon
+ *
+ * @ORM\Table(name="pokemon")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PokemonRepository")
+ */
+class Pokemon
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="number", type="integer")
+     */
+    private $number;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Pokemon
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set number
+     *
+     * @param integer $number
+     *
+     * @return Pokemon
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return int
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+}
+
--- symfony/tests/AppBundle/Controller/DefaultControllerTest.php
+++ symfony/tests/AppBundle/Controller/DefaultControllerTest.php
@@ -15,4 +15,7 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+
+
+    
 }
